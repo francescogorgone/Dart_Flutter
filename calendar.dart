c67dcaf897f79a132e1f4d0ffe58ab9d91fdc938
Francesco Gorgone <148762861+francescogorgone@users.noreply.github.com>
2024-12-06 17:44:56 +0100

    Create calendar.dart

---
+++ calendar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyScaffold(),
+    );
+  }
+}
+
+class MyScaffold extends StatefulWidget {
+  const MyScaffold({Key? key}) : super(key: key);
+
+  @override
+  _MyScaffoldState createState() => _MyScaffoldState();
+}
+
+class _MyScaffoldState extends State<MyScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Example Scaffold"),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              _selectedDate(context);
+            },
+            child: Text("Show DatePicker"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _selectTime(context){
+
+              }
+            },
+            child: Text("Show TimePicker"),
+          ),
+        ],
+      ),
+    );
+  }
+  void _selectedDate(BuildContext context) async {
+    DateTime? selectedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(), //data che verrà evidenziata, solitamente il giornoa attuale
+        firstDate: DateTime(2020),
+        lastDate: DateTime(2025),
+    );
+    print(selectedDate);
+  }
+
+  void _selectTime(BuildContext context) async {
+    TimeOfDay? selectedTime = await showTimePicker(
+    context: context,
+        initialTime: TimeOfDay.now(),
+    );
+    print(selectedTime);
+  }
+}
